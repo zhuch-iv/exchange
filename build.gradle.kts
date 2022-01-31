f3aea8efd59fc31915773e28fe5f94efab5fa12c
zhuch-iv <zhuch.dev@pm.me>
2022-02-01 00:05:36 +0300

    add sonarqube workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.sonarqube") version "3.3"
 	kotlin("jvm") version "1.4.30"
 	kotlin("plugin.spring") version "1.4.30"
 }
@@ -49,4 +50,10 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+sonarqube {
+	properties {
+		property("sonar.projectKey", "zhuch-iv_exchange")
+	}
 }
